228585457477e0e3d173942e73ab590afa5076a1
Emily Engle <emily.engle.97@gmail.com>
2023-12-06 15:22:39 -0500

    ...ugh, simplifying

--- 2023/dec-6/solution.py
+++ 2023/dec-6/solution.py
@@ -29,39 +29,22 @@ if __name__ == "__main__":
     print(prod)
 
     # Part 2
-    time = 71530
-    distance = 940200
-    # time = 48989083
-    # distance = 390110311121360
+    # time = 71530
+    # distance = 940200
+    time = 48989083
+    distance = 390110311121360
 
-    # system of equations !
-    # t1 * (t2 - t1) = 940200
-    #      - t1^2 + t1t2 - 940200 = 0
-    #      - t1^2 + (71530 - t1)t1 - 940200 = 0
-    #      - t1^2 + 71530t1 - t1^2 - 940200 = 0
-    #      - 2t1^2 + (time)t1 - distance = 0
-
-    #      - t1^2 + t1t2 - 940200 = 0
-    #      - (71530 - t2)(71530 - t2) + (71530 - t2)t2 - 940200 = 0
-    #      - (t - t2)(t - t2) + (t - t2)t2 - d = 0
-    #      - (t^2 - t2t - t2t + t2^2) + t2t - t2^2 - d = 0
-    #      - t^2 + t2t + t2t - t2^2 + t2t - t2^2 - d = 0
-    #      - 2t2^2 + (3t)t2 + (-d - t^2) = 0
-
-    # t1 + t2 = 71530
-    #     t2 = 71530 - t1
-    #     t1 = 71530 - t2
+    # quadratic equation !
+    # t1 * (t - t1) = d
+    #      -t1^2 + (t)t1 = d
+    #      t1^2 - (t)t1 + d = 0
 
     # discriminant: b^2 - 4ac
-    # print(f"Discriminant: {get_discriminant(-2, time, -1 * distance)}")
-    print(
-        f"Discriminant: {get_discriminant(-2, 3 * time, (-1 * distance) - (time * time))}"
-    )
+    print(f"Discriminant: {get_discriminant(1, time * -1, distance)}")
 
     # quadratic roots
-    # roots = get_roots(-2, time, -1 * distance)
-    roots = get_roots(-2, 3 * time, (-1 * distance) - (time * time))
+    roots = get_roots(1, time * -1, distance)
     root1 = math.ceil(min(roots))
     root2 = math.floor(max(roots))
-    print(f"Roots: {root1, root2}")
+    print(f"Roots: {roots}")
     print(f"Total ways to win: {root2 - root1 + 1}")
