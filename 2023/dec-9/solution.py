f0fec7f27ee23ec86ab7e928c124489c11702ca6
Emily Engle <emily.engle.97@gmail.com>
2023-12-10 00:12:32 -0500

    Dec 9, part 2

--- 2023/dec-9/solution.py
+++ 2023/dec-9/solution.py
@@ -1,4 +1,4 @@
-sum = 0
+prev_sum, next_sum = 0, 0
 for seq in open("input.txt").readlines():
     initial_seq = [int(s) for s in seq.strip().split(" ")]
     patterns = [initial_seq]
@@ -13,10 +13,19 @@ for seq in open("input.txt").readlines():
     for i in range(len(patterns) - 1, -1, -1):
         pattern = patterns[i]
         if i == len(patterns) - 1:
+            pattern.insert(0, 0)
+        else:
+            pattern.insert(0, pattern[0] - patterns[i + 1][0])
+
+    for i in range(len(patterns) - 1, -1, -1):
+        pattern = patterns[i]
+        if i == len(patterns) - 1:
             pattern.append(0)
         else:
             pattern.append(pattern[-1] + patterns[i + 1][-1])
 
-    sum += patterns[0][-1]
+    prev_sum += patterns[0][0]
+    next_sum += patterns[0][-1]
 
-print(sum)
+print(prev_sum)
+print(next_sum)
